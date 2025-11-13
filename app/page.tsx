0fa5a26a78b657797c2d7a806260f330ab43a493
Jomari Tiu <jomtiu16@gmail.com>
2025-11-13 20:44:35 +0800

    :lipstick: hide testimonials

--- app/page.tsx
+++ app/page.tsx
@@ -23,7 +23,7 @@ export default function Home() {
       <ServiceAreas />
       <WhyChoose />
       <BookingRates />
-      <Testimonials />
+      {/* <Testimonials /> */}
       <Contact />
       <ProfessionalCommitment />
     </main>
